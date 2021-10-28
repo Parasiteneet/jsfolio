a9276c08dbb40425563d636112452e9c88691e78
Parasiteneet <wumabeatboxer@yahoo.co.jp>
2021-10-28 19:05:58 +0900

    50

--- 040_変数/910_修了確認_falsyとtruthy/start/main.js
+++ 040_変数/910_修了確認_falsyとtruthy/start/main.js
@@ -2,15 +2,15 @@
  * 問題１：
  * 以下のコンソールにはどのような値が表示されるでしょうか？
  */
-// console.log("0 == false", 0 == false);
-// console.log("0 === false", 0 === false);
-// console.log('"false" == false', "false" == false);
-// console.log('"0" == 0', "0" == 0);
-// console.log('Boolean("0") === false', Boolean("0") === false);
-// console.log('Boolean(0) === false', Boolean(0) === false);
-// console.log('!Boolean(0) === false', !Boolean(0) === false);
-// console.log('-1 == false', -1 == false);
-// console.log('!10 === false', !10 === false);
+//console.log("0 == false", 0 == false);  //true 数値と数値
+//console.log("0 === false", 0 === false); //false 文字列と数値
+//console.log('"false" == false', "false" == false); //true == は値だけの比較
+//console.log('"0" == 0', "0" == 0); //false
+//console.log('Boolean("0") === false', Boolean("0") === false); //false
+//console.log('Boolean(0) === false', Boolean(0) === false); //false
+//console.log('!Boolean(0) === false', !Boolean(0) === false); // false
+//console.log('-1 == false', -1 == false); // true
+//console.log('!10 === false', !10 === false);// false
 
 /**
  * 問題２：
@@ -20,10 +20,11 @@
  * 以下の関数fnの初期化を適切に書き直してください。
  * ※aには0以上の整数値が渡ってくるものとします。
  */
-let a = 1;
+let a = null;
 
 function fn(num) {
-    num = num || -1;
+    if(num === undefined || num === null)
+    num = -1;
     console.log(num);
 }
 fn(a);
@@ -39,3 +40,10 @@ fn(a);
  * greeting("Bob"); -> 出力結果："hello, Bob"
  *
  */
+
+function greet(n,g) {
+    g = g || 'hello';
+    console.log(`${g}, ${n}`);
+}
+
+greet("Bob");
